6bfdbdcfe3dece9f8053919189f4661eb199de9f
Nickolas Moura <nickolas.moura@outlook.com>
2024-05-03 21:48:29 -0300

    initial commit

---
+++ dio-java-basico/README.md
@@ -0,0 +1,3 @@
+# dio-java-basico
+
+Alterando o conteúdo de um arquivo de forma local
---
+++ dio-java-basico/meu-primeiro-projeto-java/src/MinhaTurma.java
@@ -0,0 +1,9 @@
+public class MinhaTurma {
+
+    public static void main(String[] args) {
+
+        System.out.println("\nBoas vindas, Turma DIO!\n");
+
+    }
+
+}
