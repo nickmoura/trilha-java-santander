134e92f3956c72df903a9476873c5d2848edeb8e
Nickolas Moura <nickolas.moura@outlook.com>
2024-05-03 23:00:32 -0300

    'anatomia' project added

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,9 @@
+public class MinhaClasse {
+	//main é o método principal para projetos executáveis
+public static void main (String [] args) {
+	//Meu código está saindo grudado. Preciso colocar o \n
+	System.out.print ("\nOlá, turma! Sejam bem vindos.\n");
+
+}
+
+}
